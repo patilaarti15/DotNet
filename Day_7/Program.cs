e9c1f63eaee0a9cf05914234bd2acb93b1288cd2
patilaarti15 <65292226+patilaarti15@users.noreply.github.com>
2020-12-09 00:02:34 +0530

    Add files via upload

---
+++ Day_7/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentEvent06
+{
+    class EventAssignment
+    {
+        static void Main(string[] args)
+        {
+            List<Employees> li = new List<Employees>();
+
+            try
+            {
+                char choice = 'y';
+                while (choice == 'y')
+                {
+                    Employees.insertEmployee(li);
+                    Console.Write("Do you Want to continue type (y/n) : ");
+                    choice = Convert.ToChar(Console.ReadLine());
+
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please Enter Valid Input");
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+            Employees[] arr = li.ToArray();
+
+            foreach (Employees el in arr)
+            {
+                Console.WriteLine(el.EmpId + "  " + el.EmpName + "  " + el.EmpSal);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+    public delegate void InvalidEmpIdEvenHandler();
+    public delegate void InvalidEmpNameEvenHandler();
+    public delegate void InvalidEmpSalEvenHandler();
+    class Employees
+    {
+        public event InvalidEmpIdEvenHandler invalidEmpId;
+        public event InvalidEmpNameEvenHandler invalidEmpName;
+        public event InvalidEmpSalEvenHandler invalidEmpSal;
+
+        private int empId;
+
+        private string empName;
+        private decimal empSal;
+
+        public int EmpId
+        {
+            get { return empId; }
+            set
+            {
+                if (value < 10) empId = value;
+                else
+                {
+                    invalidEmpId();
+                }
+            }
+        }
+        public string EmpName
+        {
+            get { return empName; }
+            set
+            {
+                if (value == null)
+                {
+                    invalidEmpName();
+                }
+                else empName = value;
+            }
+        }
+        public decimal EmpSal
+        {
+            get { return empSal; }
+            set
+            {
+                if (value < 1000)
+                {
+                    invalidEmpSal();
+                }
+                else empSal = value;
+            }
+        }
+
+        public Employees(int empId, string empName, decimal empSal)
+        {
+            this.EmpId = empId;
+            this.EmpName = empName;
+            this.EmpSal = empSal;
+        }
+        public Employees()
+        {
+
+        }
+        public static void insertEmployee(List<Employees> li)
+        {
+            Employees emp = new Employees();
+            emp.invalidEmpId += Emp_invalidEmpId;
+            emp.invalidEmpName += Emp_invalidEmpName;
+            emp.invalidEmpSal += Emp_invalidEmpSal;
+            Console.WriteLine("-------------------------------------------------");
+            Console.Write("Enter Employee Id : ");
+            int empId = Convert.ToInt32(Console.ReadLine());
+            emp.EmpId = empId;
+            Console.Write("Enter Employee Name : ");
+            string empName = Console.ReadLine();
+            emp.EmpName = empName;
+            Console.Write("Enter Employee Sal : ");
+            decimal empSal = Convert.ToDecimal(Console.ReadLine());
+            emp.EmpSal = empSal;
+            li.Add(new Employees(empId, empName, empSal));
+            //li.Add(new Employees { EmpId = empId, EmpName = empName, EmpSal = empSal });
+        }
+
+        private static void Emp_invalidEmpSal()
+        {
+            Console.WriteLine("Sal should be Greater then 1000 !!");
+        }
+
+        private static void Emp_invalidEmpName()
+        {
+            Console.WriteLine("Name should not be Null !!");
+        }
+
+        private static void Emp_invalidEmpId()
+        {
+            Console.WriteLine("Emp Id should be Less then 10 !!");
+        }
+    }
+
+}
+
---
+++ Day_7/Program2.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment06
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            List<Employees> li = new List<Employees>();
+            Employees e = new Employees();
+            // e.EmpId = 100000;
+            try
+            {
+                char choice = 'y';
+                while (choice == 'y')
+                {
+                    Employees.insertEmployee(li);
+                    Console.Write("Do you Want to continue type (y/n) : ");
+                    choice = Convert.ToChar(Console.ReadLine());
+
+                }
+            }
+            catch (InvalidEmpIdException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (InvalidEmpNameException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (InvalidEmpSalException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please Enter Valid Input");
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+            Employees[] arr = li.ToArray();
+
+            foreach (Employees el in arr)
+            {
+                Console.WriteLine(el.EmpId + "  " + el.EmpName + "  " + el.EmpSal);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+    class Employees
+    {
+        private int empId;
+
+        private string empName;
+        private decimal empSal;
+
+        public int EmpId
+        {
+            get { return empId; }
+            set
+            {
+                if (value < 10) empId = value;
+                else
+                {
+                    Exception ex;
+                    ex = new InvalidEmpIdException("Invalid Emp Id !!");
+                    throw ex;
+                }
+            }
+        }
+        public string EmpName
+        {
+            get { return empName; }
+            set
+            {
+                if (value == null)
+                {
+                    Exception ex;
+                    ex = new InvalidEmpNameException("Invalid Name !!");
+                    throw ex;
+                }
+                else empName = value;
+            }
+        }
+        public decimal EmpSal
+        {
+            get { return empSal; }
+            set
+            {
+                if (value < 1000)
+                {
+                    Exception ex;
+                    ex = new InvalidEmpSalException("Invalid Emp Sal !!");
+                    throw ex;
+                }
+                else empSal = value;
+            }
+        }
+
+        public Employees(int empId, string empName, decimal empSal)
+        {
+            this.EmpId = empId;
+            this.EmpName = empName;
+            this.EmpSal = empSal;
+        }
+        public Employees()
+        {
+
+        }
+        public static void insertEmployee(List<Employees> li)
+        {
+            Employees emp = new Employees();
+            Console.WriteLine("-------------------------------------------------");
+            Console.Write("Enter Employee Id : ");
+            int empId = Convert.ToInt32(Console.ReadLine());
+            emp.EmpId = empId;
+            Console.Write("Enter Employee Name : ");
+            string empName = Console.ReadLine();
+            emp.EmpName = empName;
+            Console.Write("Enter Employee Sal : ");
+            decimal empSal = Convert.ToDecimal(Console.ReadLine());
+            emp.EmpSal = empSal;
+            li.Add(new Employees(empId, empName, empSal));
+            //li.Add(new Employees { EmpId = empId, EmpName = empName, EmpSal = empSal });
+        }
+
+
+
+    }
+    public class InvalidEmpIdException : ApplicationException
+    {
+        public InvalidEmpIdException(string massage) : base(massage)
+        {
+
+        }
+    }
+    public class InvalidEmpNameException : ApplicationException
+    {
+        public InvalidEmpNameException(string massage) : base(massage)
+        {
+
+        }
+    }
+    public class InvalidEmpSalException : ApplicationException
+    {
+        public InvalidEmpSalException(string massage) : base(massage)
+        {
+
+        }
+    }
+}
